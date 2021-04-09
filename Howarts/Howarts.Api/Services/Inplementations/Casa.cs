4cbfef166209e6cbb3f2de3c25863a2149ac1f16
keivervasqz <71145371+keivervasqz@users.noreply.github.com>
2021-04-09 12:16:21 -0400

    Commit

---
+++ Howarts/Howarts.Api/Services/Inplementations/Casa.cs
@@ -0,0 +1,22 @@
+﻿using Howarts.Api.Datos;
+using Howarts.Api.Context;
+using Howarts.Api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Howarts.Api.Services.Inplementations
+{
+    public class Casa : ICasa
+    {
+        private readonly HowartsContext context;
+
+        public async Task<IEnumerable<Casas>> ListarCasas()
+        {
+            return context.Casas.ToList();
+        }
+    }
+}
